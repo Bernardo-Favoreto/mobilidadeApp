0f7d4c8eb20ac541deece726e0b2340d2e03f455
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-03-10 19:19:44 -0300

    navegacao a partir da main

--- OnTime/app/src/main/java/br/com/example/ontime/MainActivity.kt
+++ OnTime/app/src/main/java/br/com/example/ontime/MainActivity.kt
@@ -1,12 +1,34 @@
 package br.com.example.ontime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun redirectEvents(view: View) {
+        val intent = Intent(this, EventActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun redirectRotaEvento(view: View) {
+        val intent = Intent(this, RotaEventoActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun redirectRestaurantes(view: View) {
+        val intent = Intent(this, RestauranteActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun redirectRotaHotal(view: View) {
+        val intent = Intent(this, RotaHotelActivity::class.java)
+        startActivity(intent)
     }
 }
--- OnTime/app/src/main/java/br/com/example/ontime/RotaHotelActivity.kt
+++ OnTime/app/src/main/java/br/com/example/ontime/RotaHotelActivity.kt
@@ -1,12 +1,20 @@
 package br.com.example.ontime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class HotelActivity : AppCompatActivity() {
+class RotaHotelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel)
     }
+
+    fun backToMain(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
