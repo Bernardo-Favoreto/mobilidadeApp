14aeb496b25ef8daae30afc9ec6349a0b443cac8
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-03-10 09:31:56 -0300

    primeiras telas em branco

---
+++ OnTime/app/src/main/java/br/com/example/ontime/EventActivity.kt
@@ -0,0 +1,12 @@
+package br.com.example.ontime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event)
+    }
+}
