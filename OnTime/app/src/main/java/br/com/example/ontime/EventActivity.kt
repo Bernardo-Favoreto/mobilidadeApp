872049589407e1b255913bbd5a23e627b138f92f
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-03-11 20:16:08 -0300

    tela eventos adicionada

--- OnTime/app/src/main/java/br/com/example/ontime/EventActivity.kt
+++ OnTime/app/src/main/java/br/com/example/ontime/EventActivity.kt
@@ -1,12 +1,18 @@
 package br.com.example.ontime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class EventActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
+    }
+    fun backToMain(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
